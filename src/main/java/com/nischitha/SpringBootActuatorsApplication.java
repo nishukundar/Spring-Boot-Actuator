2852199284987048f3b3763bd2717edfbc658ded
nishukundar <nischithasrinivas83@gmail.com>
2024-09-17 19:07:31 +0530

    initial commit

---
+++ src/main/java/com/nischitha/SpringBootActuatorsApplication.java
@@ -0,0 +1,13 @@
+package com.nischitha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootActuatorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootActuatorsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nischitha/SpringBootActuatorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nischitha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootActuatorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
